e27286cea80fa2e4c775919d8fa62dfeb10620d7
Kostyantyn Zagorodniy <korvin32@yahoo.com>
2013-03-25 15:57:11 +0100

    Customers management added

---
+++ src/webpkg1/CustomerBean.java
@@ -0,0 +1,72 @@
+package webpkg1;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import session.CustomerManager;
+import entity.Address;
+import entity.Customer;
+
+@ManagedBean(name="customer")
+@RequestScoped
+public class CustomerBean {
+	
+	@EJB
+	private CustomerManager customerManager;
+
+	private HttpSession session;
+	
+	@ManagedProperty(value="#{webBean1}")
+	private WebBean1 webBean1;
+	
+	private String rePassword;
+	private Customer customer;
+	private Address address;
+	
+	public CustomerBean() {
+		customer = new Customer();
+		address = new Address();
+		session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+	}
+	
+	public String register() {
+		if (!customer.getPassword().equals(rePassword)) {
+			return null;
+		}
+		int customerId = customerManager.registerCustomer(customer, address);
+		if (customerId > 0) {
+			session.setAttribute("customer", customer);
+			return "index";
+		} else {
+			return "error";
+		}
+	}
+
+	public String getRePassword() {
+		return rePassword;
+	}
+
+	public void setRePassword(String rePassword) {
+		this.rePassword = rePassword;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
