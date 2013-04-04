d326d5cf4f62687298462c080d7242c662b7dc62
Kostyantyn Zagorodniy <korvin32@yahoo.com>
2013-04-04 16:21:36 +0200

    Exceptions handling

--- src/webpkg1/LoginBean.java
+++ src/webpkg1/LoginBean.java
@@ -35,16 +35,22 @@ public class LoginBean implements Serializable {
 	}
 	
 	public String doLogin() {
-		Customer customer = customerManager.checkLoginData(login, password);
-		if (customer != null) {
-			session.setAttribute("customer", customer);
-			webBean1.setLoggedIn(true);
-			webBean1.setLoggedinCustomer(customer);
-			return "index";
-		} else {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No such User or wrong password entered! Please try again", null));
+		try {
+			Customer customer = customerManager.checkLoginData(login, password);
+			if (customer != null) {
+				session.setAttribute("customer", customer);
+				webBean1.setLoggedIn(true);
+				webBean1.setLoggedinCustomer(customer);
+				return "index";
+			} else {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "No such User or wrong password entered! Please try again", null));
+				return null;
+			}
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, e.getMessage(), null));
 			return null;
 		}
+		
 	}
 
 	public WebBean1 getWebBean1() {
